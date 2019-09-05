aec5b3db34af1fa1f8f5775dc5c0479c8f47c282
AlekseyKireychik <kireychika@mail.ru>
2019-09-05 23:25:04 +0300

    add rect-router-dom

--- zdorovayasemia/src/App.js
+++ zdorovayasemia/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> zd s.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn sdsdsdsd
         </a>
       </header>
     </div>
