304854c666bd3be18988b62dab0968d783718a7e
AlekseyKireychik <kireychika@mail.ru>
2019-09-09 21:55:57 +0300

    add

--- zdorovayasemia/src/App.js
+++ zdorovayasemia/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> zd s.
+          Edit <code>src/App.js</code> test.
         </p>
         <a
           className="App-link"
