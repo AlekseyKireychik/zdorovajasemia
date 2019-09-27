33e63f89247344d2b7cbeecb912df10049fba447
unknown <you@example.com>
2019-09-27 18:00:52 +0300

    add

--- zdorovayasemia/src/components/navigation/navigation.jsx
+++ zdorovayasemia/src/components/navigation/navigation.jsx
@@ -5,13 +5,13 @@ import style from './Navigation.module.css';
 const Navigation = () => (
   <div className={style.navigation}>
       <ul className={style.nav}>
-        <li><NavLink to="/home">Главная</NavLink></li>
+        <li><NavLink to="/home" activeClassName={style.active}>Главная</NavLink></li>
         <li><NavLink to="/about">О нас</NavLink></li>
         <li><NavLink to="/services">Услуги</NavLink></li>
-        <li><NavLink to="/input">Input</NavLink></li>
-        <li><NavLink to="/tabbar">Tabbar</NavLink></li>
-        <li><NavLink to="/tooltip">Наша команда</NavLink></li>
-        <li><NavLink to="/modal">Отзывы</NavLink></li>
+        <li><NavLink to="/products">Массажные коврики</NavLink></li>        
+        <li><NavLink to="team">Наша команда</NavLink></li>
+        <li><NavLink to="/reviews">Отзывы</NavLink></li>
+        <li><NavLink to="/contacts">Контакты</NavLink></li>
       </ul>
   </div>
 );
