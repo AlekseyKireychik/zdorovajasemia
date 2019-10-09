ec059925d2d445ca9a124f132cadea19b241abb8
unknown <you@example.com>
2019-10-09 11:17:27 +0300

    add icon

--- zdorovayasemia/src/components/footer/Footer.jsx
+++ zdorovayasemia/src/components/footer/Footer.jsx
@@ -12,32 +12,45 @@ const Footer = props => {
       <div className={style.container}>
         <div className={style.adresslist}>
           <span>Массажно-оздоровительный центр «Здоровая семья»</span>
-          <Icon linkImg="/images/location-pin.svg" altText="Адрес" />  
-          <Adress adress="г.Брест, ул.Орджоникидзе, 39" />
+          <div className={style.adressItem}>
+            <Icon linkImg="/images/icons/location-pin.svg" altText="Адрес" />
+            <Adress adress="г.Брест, ул.Орджоникидзе, 39" />
+          </div>
         </div>
         <ul className={style.contactList}>
           <li className={style.contactItem}>
-            <Icon linkImg="/images/location-pin.svg" altText="Почтовый адрес" />  
+            <Icon linkImg="/images/icons/mail.svg" altText="Почтовый адрес" />
             <Email email="zdorovayasemia@gmail.com" />
           </li>
           <li className={style.contactItem}>
-            <Icon linkImg="/images/phone.svg" altText="Телефон" />
+            <Icon linkImg="/images/icons/phone.svg" altText="Телефон" />
             <Phone numberhref="+375298296956" number="+375 (29) 829 69 56" />
           </li>
           <li className={style.contactItem}>
-            <Icon linkImg="/images/phone.svg" altText="Телефон" />
+            <Icon linkImg="/images/icons/phone.svg" altText="Телефон" />
             <Phone numberhref="+375162534895" number="+375 (16) 253 48 95" />
           </li>
         </ul>
         <ul className={style.socialList}>
           <li className={style.socialItem}>
-            <Social link="#"  linkImg="#" nameImg="vk"/>
+            <Social 
+              link="#" 
+              linkImg="/images/icons/vk.svg" 
+              nameImg="vk" />
           </li>
           <li className={style.socialItem}>
-            <Social link="#"  linkImg="#" nameImg="instagram"/>
+            <Social
+              link="#"
+              linkImg="/images/icons/insta.svg"
+              nameImg="instagram"
+            />
           </li>
           <li className={style.socialItem}>
-            <Social link="#"  linkImg="#" nameImg="odnoklassniki"/>
+            <Social
+              link="#"
+              linkImg="/images/icons/ok.svg"
+              nameImg="ok"
+            />
           </li>
         </ul>
       </div>
