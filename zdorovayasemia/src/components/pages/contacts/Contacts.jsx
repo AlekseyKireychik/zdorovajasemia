6b2573ab91d655686f9fe2a388de68b33098a154
unknown <you@example.com>
2019-10-21 17:52:49 +0300

    add new component rating. update contact page

--- zdorovayasemia/src/components/pages/contacts/Contacts.jsx
+++ zdorovayasemia/src/components/pages/contacts/Contacts.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import InnerBanner from "../../innerBanner/InnerBanner";
 import Form from "../../form/Form";
-import Image from "../../image/Image";
 import Phone from "../../phone/Phone";
+import { YMaps, Map, Placemark } from "react-yandex-maps";
 import style from "./Contacts.module.css";
+
+const mapState = {
+  center: [52.1008174, 23.6916544],
+  zoom: 18,
+  controls: ["zoomControl"]
+};
+const contactPageInfo = {
+  title: "Свяжитесь с нами",
+  subTitle:
+    "Если вы хотите связаться с нами, воспользуйтесь формой. Если вы предпочитаете телефонный разговор, вы можете позвонить сейчас по номерам:",
+  text: "Мы всегда стремимся ответить на ваши вопросы быстро и эффективно."
+};
 
 const Contacts = props => {
   return (
@@ -13,24 +25,20 @@ const Contacts = props => {
         children="Контакты"
       />
       <div className={style.container}>
-        <div className={style.left}>
+        <div className={style.top}>
           <div className={style.textWraper}>
-            <h2 className={style.Subtitle}>Свяжитесь с нами</h2>
-            <p>
-              Если вы хотите связаться с нами, воспользуйтесь формой. Если вы
-              предпочитаете телефонный разговор, вы можете позвонить сейчас по
-              номерам:
-            </p>
+            <h2 className={style.Subtitle}>{contactPageInfo.title}</h2>
+            <p>{contactPageInfo.subTitle}</p>
             <Phone numberhref="+375298296956" number="+375 (29) 829 69 56" />
             <Phone numberhref="+375162534895" number="+375 (16) 253 48 95" />
-            <p>
-              Мы всегда стремимся ответить на ваши вопросы быстро и эффективно.
-            </p>
+            <p>{contactPageInfo.text}</p>
           </div>
           <Form />
         </div>
-        <div className={style.right}>
-          <Image width="600" alt="map" />
+        <div className={style.bootom}>
+          <YMaps query={{ load: "control.ZoomControl" }}>
+            <Map width="1200px" height="450px" state={mapState} />
+          </YMaps>
         </div>
       </div>
     </React.Fragment>
--- zdorovayasemia/src/components/phone/Phone.jsx
+++ zdorovayasemia/src/components/phone/Phone.jsx
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Phone.css";
 
+// const numberPhone = {
+//   numberOne: "+375 (29) 829 69 56",
+//   numberTwo: "+375 (16) 253 48 95"
+// };
+
 const Phone = props => {
-  return (
-    <a href={"tel:" + (props.numberhref) }>
-      {props.number}
-    </a>
-  );
+  return <a href={"tel:" + props.numberhref}>{props.number}</a>;
 };
 
 export default Phone;
