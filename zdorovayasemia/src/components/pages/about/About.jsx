85a544e123f181dc58c306f4daa021d4ffd49470
unknown <you@example.com>
2019-10-29 15:45:16 +0300

    update about page

--- zdorovayasemia/src/components/pages/about/About.jsx
+++ zdorovayasemia/src/components/pages/about/About.jsx
@@ -1,46 +1,112 @@
 import React from "react";
 import InnerBanner from "../../innerBanner/InnerBanner";
+import Icon from "../../icon/Icon";
 import Store from "../../../store";
 import style from "./About.module.css";
-
-
 
 const About = props => {
   return (
     <React.Fragment>
-      <InnerBanner bgimage={Store.pageAbout.info.background} 
-      children={Store.pageAbout.info.title} />
+      <InnerBanner
+        bgimage={Store.pageAbout.info.background}
+        children={Store.pageAbout.info.title}
+      />
       <div className={style.container}>
         <h2>{Store.pageAbout.greeting.title}</h2>
         <p>{Store.pageAbout.greeting.subTitle}</p>
-        <section>
-          <div className={style.wrap}>
+        <section className={style.item}>
+          <div className={style.wrapText}>
             <h3>Для детей:</h3>
             <ul>
-              <li>Лечебный и профилактический массаж (с 2-х месяцев)</li>
-              <li>Оздоровительная гимнастика</li>
-              <li>Невролог</li>
-              <li>Тейпирование</li>
-              <li>Массажные коврики, стельки, подушки для новорожденных</li>
+              <li>
+                <Icon
+                  width="40px"
+                  alt="icon-ok"
+                  src="..//images/icons/icon-ok.svg"
+                />
+                Лечебный и профилактический массаж (с 2-х месяцев)
+              </li>
+              <li>
+                <Icon
+                  width="40px"
+                  alt="icon-ok"
+                  src="..//images/icons/icon-ok.svg"
+                />
+                Оздоровительная гимнастика
+              </li>
+              <li>
+                <Icon
+                  width="40px"
+                  alt="icon-ok"
+                  src="..//images/icons/icon-ok.svg"
+                />
+                Невролог
+              </li>
+              <li>
+                <Icon
+                  width="40px"
+                  alt="icon-ok"
+                  src="..//images/icons/icon-ok.svg"
+                />
+                Тейпирование
+              </li>
+              <li>
+                <Icon
+                  width="40px"
+                  alt="icon-ok"
+                  src="..//images/icons/icon-ok.svg"
+                />
+                Массажные коврики, стельки, подушки для новорожденных
+              </li>
             </ul>
           </div>
-          <div><img src="..//images/fotobaby.jpg" alt=""/></div>
+          <div className={style.wrapImg}>
+            <img src="..//images/fotobaby.jpg" alt="" />
+          </div>
         </section>
-        <section>
-        <div><img src="..//images/massaz_osteopat.jpg" alt=""/></div>
-        <h3>Для взрослых:</h3>
-        <ul>          
-          <li>
-            Лечебный, профилактический, антицеллюлитный, фитнес-формирующий,
-            массаж лица, общий, массаж бамбуковыми палочками, арома-массаж,
-          </li>
-          <li> "Антистресс" и др.</li>
-          <li>Тейпирование</li>
-          <li>
-            Подарочные сертификаты (на сумму, по программе, индивидуальная
-            разработка программы)
-          </li>
-        </ul>
+        <section className={style.item}>
+          <div className={style.wrapImg}>
+            <img src="..//images/massaz_osteopat.jpg" alt="" />
+          </div>
+          <div className={style.wrapText}>
+            <h3>Для взрослых:</h3>
+            <ul>
+              <li>
+                <Icon
+                  width="40px"
+                  alt="icon-ok"
+                  src="..//images/icons/icon-ok.svg"
+                />
+                Лечебный, профилактический, антицеллюлитный, фитнес-формирующий,
+                массаж лица, общий, массаж бамбуковыми палочками, арома-массаж,
+              </li>
+              <li>
+                <Icon
+                  width="40px"
+                  alt="icon-ok"
+                  src="..//images/icons/icon-ok.svg"
+                />
+                "Антистресс" и др.
+              </li>
+              <li>
+                <Icon
+                  width="40px"
+                  alt="icon-ok"
+                  src="..//images/icons/icon-ok.svg"
+                />
+                Тейпирование
+              </li>
+              <li>
+                <Icon
+                  width="40px"
+                  alt="icon-ok"
+                  src="..//images/icons/icon-ok.svg"
+                />
+                Подарочные сертификаты (на сумму, по программе, индивидуальная
+                разработка программы)
+              </li>
+            </ul>
+          </div>
         </section>
         <p>{Store.pageAbout.greeting.resume}</p>
       </div>
