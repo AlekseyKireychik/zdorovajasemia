4c25cf084e587c65c2e001c7119949b0c2bf81fe
unknown <you@example.com>
2019-11-20 10:41:13 +0300

    update list in about us page

--- zdorovayasemia/src/components/pages/about/About.jsx
+++ zdorovayasemia/src/components/pages/about/About.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import InnerBanner from "../../innerBanner/InnerBanner";
-import Icon from "../../icon/Icon";
 import Store from "../../../store";
 import style from "./About.module.css";
 
@@ -18,46 +17,11 @@ const About = props => {
           <div className={style.wrapText}>
             <h3>Для детей:</h3>
             <ul>
-              <li>
-                <Icon
-                  width="40px"
-                  alt="icon-ok"
-                  src="..//images/icons/icon-ok.svg"
-                />
-                Лечебный и профилактический массаж (с 2-х месяцев)
-              </li>
-              <li>
-                <Icon
-                  width="40px"
-                  alt="icon-ok"
-                  src="..//images/icons/icon-ok.svg"
-                />
-                Оздоровительная гимнастика
-              </li>
-              <li>
-                <Icon
-                  width="40px"
-                  alt="icon-ok"
-                  src="..//images/icons/icon-ok.svg"
-                />
-                Невролог
-              </li>
-              <li>
-                <Icon
-                  width="40px"
-                  alt="icon-ok"
-                  src="..//images/icons/icon-ok.svg"
-                />
-                Тейпирование
-              </li>
-              <li>
-                <Icon
-                  width="40px"
-                  alt="icon-ok"
-                  src="..//images/icons/icon-ok.svg"
-                />
-                Массажные коврики, стельки, подушки для новорожденных
-              </li>
+              <li>Лечебный и профилактический массаж (с 2-х месяцев)</li>
+              <li>Оздоровительная гимнастика</li>
+              <li>Невролог</li>
+              <li>Тейпирование</li>
+              <li>Массажные коврики, стельки, подушки для новорожденных</li>
             </ul>
           </div>
           <div className={style.wrapImg}>
@@ -72,43 +36,19 @@ const About = props => {
             <h3>Для взрослых:</h3>
             <ul>
               <li>
-                <Icon
-                  width="40px"
-                  alt="icon-ok"
-                  src="..//images/icons/icon-ok.svg"
-                />
                 Лечебный, профилактический, антицеллюлитный, фитнес-формирующий,
                 массаж лица, общий, массаж бамбуковыми палочками, арома-массаж,
               </li>
+              <li>"Антистресс" и др.</li>
+              <li>Тейпирование</li>
               <li>
-                <Icon
-                  width="40px"
-                  alt="icon-ok"
-                  src="..//images/icons/icon-ok.svg"
-                />
-                "Антистресс" и др.
-              </li>
-              <li>
-                <Icon
-                  width="40px"
-                  alt="icon-ok"
-                  src="..//images/icons/icon-ok.svg"
-                />
-                Тейпирование
-              </li>
-              <li>
-                <Icon
-                  width="40px"
-                  alt="icon-ok"
-                  src="..//images/icons/icon-ok.svg"
-                />
                 Подарочные сертификаты (на сумму, по программе, индивидуальная
                 разработка программы)
               </li>
             </ul>
           </div>
         </section>
-        <p>{Store.pageAbout.greeting.resume}</p>
+        <p><b>{Store.pageAbout.greeting.resume}</b></p>
       </div>
     </React.Fragment>
   );
