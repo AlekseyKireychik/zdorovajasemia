2510c24cccaa50909bc3e6afc9d974660820431e
unknown <you@example.com>
2020-02-20 18:07:58 +0300

    add admin pages

--- zdorovayasemia/src/admin/Admin.jsx
+++ zdorovayasemia/src/admin/Admin.jsx
@@ -1,25 +1,40 @@
 import React, { Fragment } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Pages from "./pages/Pages";
+import Library from "./library/Library";
+import Enquiries from "./enquiries/Enquiries";
+import Reviews from "./reviews/Reviews";
+import Setings from "./setings/Setings";
+import { NavLink } from "react-router-dom";
 import style from "./Admin.module.css";
 
 const Admin = () => {
   return (
     <Fragment>
-      <header className={style.header}> 
+      <header className={style.header}>
         <h2 className={style.title}>Cистема управления контентом</h2>
         <div className="">Выйти</div>
       </header>
       <main className={style.adminMain}>
         <nav className={style.nav}>
-          <ul>
-            <li>Страницы</li>
-            <li>Библиотека изображений</li>
-            <li>Сообщения</li>
-            <li>Отзывы</li>
-            <li>Редиректы</li>
-            <li>Настройки</li>
+          <ul>          
+            <li><NavLink to="/admin/pages">Страницы</NavLink></li>
+            <li><NavLink to="/admin/library">Библиотека изображений</NavLink></li>
+            <li><NavLink to="/admin/enquiries">Сообщения</NavLink></li>
+            <li><NavLink to="/admin/reviews">Отзывы</NavLink></li>
+            <li><NavLink to="/admin/setings" activeClassName={style.active}>Настройки</NavLink></li>
           </ul>
         </nav>
-        <article>Admin</article>
+        <BrowserRouter>
+          <article>
+            <Route path="/admin/pages" component={Pages} />
+            <Route path="/admin/library" component={Library} />
+            <Route path="/admin/enquiries" component={Enquiries} />
+            <Route path="/admin/reviews" component={Reviews} />
+            <Route path="/admin/setings" component={Setings} />
+          </article>
+        </BrowserRouter>
       </main>
     </Fragment>
   );
